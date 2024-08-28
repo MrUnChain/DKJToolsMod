acc46b62278846ca77630fea84487c82c6d62f50
MrUnChain <taichi_nabe0827@docomo.ne.jp>
2024-08-28 23:55:05 +0900

    DkjMain.javaに竹内インゴット（仮）登録

--- src/main/java/com/DKJ/DKJtools/regi/tab/DkjMain.java
+++ src/main/java/com/DKJ/DKJtools/regi/tab/DkjMain.java
@@ -1,11 +1,14 @@
 package com.DKJ.DKJtools.regi.tab;
 
+import com.DKJ.DKJtools.regi.DkjToolsItems;
+
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class DkjMain {
 
     public static final Item[] items = {
+        DkjToolsItems.TAKE_INGOT.get(),
         Items.CHERRY_PLANKS
     };
 }
